082817784332582f66678268bb6787b94cc6f517
Zara Kovalenko <zara.forgest@gmail.com>
2023-10-13 21:11:33 +0700

    hello world

---
+++ src/app/appEntry.tsx
@@ -0,0 +1,8 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <p>Hello world!</p>
+  </StrictMode>
+);
